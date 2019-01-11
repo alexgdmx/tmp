51cac0046e6a6ea7eca0660665d8a4979035f8ca
Alejandro Guadarrama <alexgdmx@gmail.com>
2019-01-11 18:39:07 +0000

    add

---
+++ alula.com.ec/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Alula Ecuador</title>
+	<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+</head>
+<body>
+	<p>Listo para comenzar</p>
+</body>
+</html>
